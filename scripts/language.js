24ea9045ef370a20934bf17d90c9bddea4cb146b
Devin Miller <edwin0258@gmail.com>
2016-05-28 01:14:41 +0000

    Much styling, Much awesomeness

--- scripts/language.js
+++ scripts/language.js
@@ -1,4 +1,8 @@
+var colors = ["#5BC0EB","#FDE74C","#9BC53D","#E55934","#FA7921"];
+
+
 $(document).ready(function(){
+    
     var width = 0;
     $("header h1").hide();
     $("header h1").fadeIn("slow");
@@ -13,6 +17,14 @@ $(document).ready(function(){
         $(".top_nav").toggle(300);
         
     })
-    
-    
+    $(".language").each(function(){
+        var color = colors[Math.floor(Math.random()*colors.length)];
+        $(this).css({"border-top-color": color});
+        $(this).on("mouseover", function(){
+            $(this).css({backgroundColor: color});
+        })
+        $(this).on("mouseleave", function(){
+            $(this).css({"background-color": "white"});
+        })
+    })
 })
